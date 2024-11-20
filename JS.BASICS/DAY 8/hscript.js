a5ad013b7f979afa61cb76bb991ee811351949ad
Hephzy-d <159773881+Hephzy-d@users.noreply.github.com>
2024-11-20 12:14:53 +0100

    third_commit

--- JS.BASICS/DAY 8/hscript.js
+++ JS.BASICS/DAY 8/hscript.js
@@ -1,11 +1,37 @@
-let cart=[];
-let item=[]
-function additems(items){
-    cart.push=(items);
-additems("laptop");
-additems("iphones");
-additems("ipad");
-console.log(cart);
+let cart = [];
+
+function addItem(item) {
+    cart.push(item);
+    console.log(`${item} has been added to the cart.`);
 }
+
+function removeItem(index) {
+    if (index >= 0 && index < cart.length) {
+        let removedItem = cart.splice(index, 1);
+        console.log(`${removedItem} has been removed from the cart.`);
+    } else {
+        console.log("Invalid index.");
+    }
+}
+
+function viewCart() {
+    console.log("Your cart contains:");
+    cart.forEach((item, index) => {
+        console.log(`${index + 1}: ${item}`);
+    });
+}
+
+function clearCart() {
+    cart = [];
+    console.log("Your cart is now empty.");
+}
+
+// Example Usage
+addItem("Laptop");
+addItem("Phone");
+viewCart();
+removeItem(0);
+viewCart();
+clearCart();
 
 
